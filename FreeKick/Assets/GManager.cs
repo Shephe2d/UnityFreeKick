191c0e70a57f6cccf1132171a9216a4bdf71d22d
Shephe2d <44551118+Shephe2d@users.noreply.github.com>
2018-11-18 20:27:16 +0800

    20181118
    20181118

---
+++ FreeKick/Assets/GManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GManager : MonoBehaviour {
+    public GameObject B_Luo_playerObj;
+	// Use this for initialization
+	void Start () {
+        GSetting.StrCurPlayerName = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void SetPlayerObj(string Name)
+    {
+        if (Name == GSetting.StrCurPlayerName) {
+            return;
+        } else if (Name==GSetting.scorePlayerName_1) {
+            Destroy(GSetting.ObjCurPlayer);
+            Vector3 pos = new Vector3(200f, 60.5f, -5f);
+            Instantiate(B_Luo_playerObj, pos,Quaternion.identity);
+            GSetting.StrCurPlayerName = GSetting.scorePlayerName_1;
+        }
+
+    }
+
+   public void SetBLUO() {
+        SetPlayerObj(GSetting.scorePlayerName_1);
+    }
+
+}
---
+++ FreeKick/Assets/GSetting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GSetting : MonoBehaviour {
+
+    public static string scorePlayerName_1 = "B罗";
+    public static string scorePlayerName_2 = "梅西";
+
+    public static string StrCurPlayerName;
+    public static GameObject ObjCurPlayer;
+    public static string StrCurBallName;
+
+    public class PlayerObject
+    {
+        GameObject obj;
+        string Name;
+        int Power;
+        int Master;
+        int accuracy;
+    }
+
+    public class BallObejct
+    {
+        GameObject obj;
+        int Speed;
+        int Weight;
+        //int ;
+    }
+
+    public class GoalKeeperObject
+    {
+        GameObject obj;
+    }
+
+}
